9cb08c477be5bbb4c4470abacae72b7553be64b1
Mamidala Bhanu Prasad <mamidala.bhanuprasad@gmail.com>
2025-08-30 14:26:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from demographic_data_analyzer import *
+
+print("Race count:\n", race_count)
+print("Average age of men:", average_age_men)
+print("Percentage with Bachelors:", percentage_bachelors)
+print("Higher education rich %:", higher_education_rich)
+print("Lower education rich %:", lower_education_rich)
+print("Minimum work hours:", min_work_hours)
+print("Rich percentage among min workers:", rich_percentage)
+print("Highest earning country:", highest_earning_country)
+print("Highest earning country percentage:", highest_earning_country_percentage)
+print("Top occupation in India for >50K:", top_IN_occupation)
