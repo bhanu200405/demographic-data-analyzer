7f1362ac3719d66fb6292cb3f78ff6e5b3a8aa20
Mamidala Bhanu Prasad <mamidala.bhanuprasad@gmail.com>
2025-08-30 14:37:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from demographic_data_analyzer import *
 
-print("Race count:\n", race_count)
-print("Average age of men:", average_age_men)
-print("Percentage with Bachelors:", percentage_bachelors)
-print("Higher education rich %:", higher_education_rich)
-print("Lower education rich %:", lower_education_rich)
-print("Minimum work hours:", min_work_hours)
-print("Rich percentage among min workers:", rich_percentage)
-print("Highest earning country:", highest_earning_country)
-print("Highest earning country percentage:", highest_earning_country_percentage)
-print("Top occupation in India for >50K:", top_IN_occupation)
+def main():
+    print("Race count:\n", race_count, "\n")
+    print("Average age of men:", average_age_men, "\n")
+    print("Percentage with Bachelor's degrees:", percentage_bachelors, "\n")
+    print("Percentage with higher education earning >50K:", higher_education_rich, "\n")
+    print("Percentage without higher education earning >50K:", lower_education_rich, "\n")
+    print("Minimum work hours per week:", min_work_hours, "\n")
+    print("Percentage of rich among those who work minimum hours:", rich_percentage, "\n")
+    print("Country with highest % earning >50K:", highest_earning_country)
+    print("Highest percentage:", highest_earning_country_percentage, "\n")
+    print("Top occupation in India for >50K:", top_IN_occupation, "\n")
+
+if __name__ == "__main__":
+    main()
