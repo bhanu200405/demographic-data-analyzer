37f9c440a24f165bf671e0df52775667257f3a5f
Mamidala Bhanu Prasad <mamidala.bhanuprasad@gmail.com>
2025-08-30 15:11:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from demographic_data_analyzer import calculate_demographic_data
+
+def main():
+    # Run analysis and print results
+    calculate_demographic_data(print_data=True)
+
+if __name__ == "__main__":
+    main()
